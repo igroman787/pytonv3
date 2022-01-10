9f8281f17c7d0b202c924bd99bedb239c3bd9a72
Eugeny Kluev <me@kluev.eu>
2022-01-11 02:55:07 +0300

    Revert "high load wallets fix #2"
    This reverts commit ad89b93f09ef60a3571df55c707b822765bc2e3a.

--- pyTON/client.py
+++ pyTON/client.py
@@ -195,13 +195,16 @@ class TonlibClient:
 
                   try:
                       if "msg_data" in ot:
-                          msg_cell_boc = codecs.decode(codecs.encode(o["msg_data"]["body"], 'utf8'), 'base64')
-                          message_cell = deserialize_boc(msg_cell_boc)
-                          msg = message_cell.data.data.tobytes()
-                          o["message"] = codecs.decode(codecs.encode(msg, 'base64'), "utf8")
+                          if ot["msg_data"]["text"]:
+                              msg_cell_boc = codecs.decode(codecs.encode(ot["msg_data"]["text"], 'utf8'), 'base64')
+                              ot["message"] = msg_cell_boc.decode("utf-8")
+                          else:
+                              msg_cell_boc = codecs.decode(codecs.encode(o["msg_data"]["body"], 'utf8'), 'base64')
+                              message_cell = deserialize_boc(msg_cell_boc)
+                              msg = message_cell.data.data.tobytes()
+                              o["message"] = codecs.decode(codecs.encode(msg, 'base64'), "utf8")
                   except:
-                      msg_cell_boc = codecs.decode(codecs.encode(ot["msg_data"]["text"], 'utf8'), 'base64')
-                      ot["message"] = msg_cell_boc.decode("utf-8")
+                      ot["message"] = ""
 
           # incoming transaction
           if not t['out_msgs']:
@@ -212,13 +215,16 @@ class TonlibClient:
 
               try:
                   if "msg_data" in t["in_msg"]:
-                      msg_cell_boc = codecs.decode(codecs.encode(t["in_msg"]["msg_data"]["body"], 'utf8'), 'base64')
-                      message_cell = deserialize_boc(msg_cell_boc)
-                      msg = message_cell.data.data.tobytes()
-                      t["in_msg"]["message"] = codecs.decode(codecs.encode(msg, 'base64'), "utf8")
+                      if t["in_msg"]["msg_data"]["text"]:
+                          msg_cell_boc = codecs.decode(codecs.encode(t["in_msg"]["msg_data"]["text"], 'utf8'), 'base64')
+                          t["in_msg"]["message"] = msg_cell_boc.decode("utf-8")
+                      else:
+                          msg_cell_boc = codecs.decode(codecs.encode(t["in_msg"]["msg_data"]["body"], 'utf8'), 'base64')
+                          message_cell = deserialize_boc(msg_cell_boc)
+                          msg = message_cell.data.data.tobytes()
+                          t["in_msg"]["message"] = codecs.decode(codecs.encode(msg, 'base64'), "utf8")
               except:
-                  msg_cell_boc = codecs.decode(codecs.encode(t["in_msg"]["msg_data"]["text"], 'utf8'), 'base64')
-                  t["in_msg"]["message"] = msg_cell_boc.decode("utf-8")
+                  t["in_msg"]["message"] = ""
         except Exception as e:
             print("getTransaction exception", e)
 
